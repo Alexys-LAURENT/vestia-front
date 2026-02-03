77657f9108beb8607d04ad8d937d68e37326dae0
Alexys LAURENT <alexyslaurent.442@gmail.com>
2026-02-03 09:52:12 +0100

    Removed useless components

--- components/ui/index.ts
+++
@@ -1,7 +0,0 @@
-export { Button } from './button';
-export { Card } from './card';
-export { Collapsible } from './collapsible';
-export { Header } from './header';
-export { IconSymbol } from './icon-symbol';
-export { Input } from './input';
-
